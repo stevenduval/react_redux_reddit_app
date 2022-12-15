87b51482003f27e91aebf874c2ef01484f94e613
Steven Duval <stevenduval@gmail.com>
2022-12-14 21:49:53 -0600

    add lazy load image component

--- src/features/Posts/Posts.js
+++ src/features/Posts/Posts.js
@@ -1,8 +1,9 @@
-import { Card, CardMedia, CardContent, Grid, Link, Typography } from "@mui/material";
+import { Card, CardContent, Grid, Link, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import Moment from "react-moment";
 import { fetchComments } from "../../app/redditDataSlice";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 export const Posts = (props) => {
 
@@ -20,14 +21,7 @@ export const Posts = (props) => {
         <Grid item xs={12} md={6} >
             <Card
                 sx={{ height: '100%', display: 'flex', flexDirection: 'column' }} onClick={() => handleClick(index, data.permalink) }>
-                <CardMedia
-                    component="img"
-                    sx={{
-                        maxHeight: '150px',
-                    }}
-                    image={data.url_overridden_by_dest}
-                    alt="random"
-                />
+                 <LazyLoadImage src={data.url_overridden_by_dest} alt="Image Alt" style={{maxHeight: '175px', objectFit: 'cover'}}/>
                 <CardContent sx={{ flexGrow: 1 }}>
                     <Typography gutterBottom variant="h6" component="h2" noWrap={true}>
                         {data.title}
--- src/features/Posts/PostsLoading.js
+++ src/features/Posts/PostsLoading.js
@@ -3,8 +3,8 @@ import { Card, CardContent, Grid, Skeleton } from "@mui/material";
 export const PostsLoading = () => {
     const posts = [1, 2, 3, 4, 5, 6];
     return (
-        posts.map(post => {
-           return ( <Grid item xs={12} md={6} >
+        posts.map((post, index) => {
+           return ( <Grid item xs={12} md={6} key={index}>
             <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }} >
                 <CardContent sx={{ flexGrow: 1 }}>
                 <Skeleton variant="rectangular" height={175} width={'100%'}/>
